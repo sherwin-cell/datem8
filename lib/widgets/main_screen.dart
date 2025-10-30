a23b257f515f577708d1093b5d2b4ece77c23681
sherwin-cell <sherwinescanilla18@gmail.com>
2025-10-30 13:05:40 +0800

    fixing the profile icon

--- lib/widgets/main_screen.dart
+++ lib/widgets/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:datem8/services/cloudinary_service.dart';
 import 'package:datem8/views/home/home_page.dart';
 import 'package:datem8/views/chat/chat_page.dart';
@@ -17,7 +19,6 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
-
   late final List<Widget> _pages;
 
   @override
@@ -32,26 +33,60 @@ class _MainScreenState extends State<MainScreen> {
     ];
   }
 
+  Future<String?> _getProfilePic() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return null;
+
+    final doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+
+    if (!doc.exists) return null;
+    return doc.data()?['profilePic'] as String?;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: _pages[_currentIndex]),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        showSelectedLabels: false, // hides selected label
-        showUnselectedLabels: false, // hides unselected labels
-        items: const [
-          BottomNavigationBarItem(icon: Icon(AppIcons.home), label: ''),
-          BottomNavigationBarItem(icon: Icon(AppIcons.chat), label: ''),
-          BottomNavigationBarItem(icon: Icon(AppIcons.explore), label: ''),
-          BottomNavigationBarItem(icon: Icon(AppIcons.friends), label: ''),
-          BottomNavigationBarItem(icon: Icon(AppIcons.profile), label: ''),
-        ],
-      ),
+    return FutureBuilder<String?>(
+      future: _getProfilePic(),
+      builder: (context, snapshot) {
+        final profilePic = snapshot.data;
+        final imageToShow = (profilePic != null && profilePic.isNotEmpty)
+            ? profilePic
+            : "https://res.cloudinary.com/dlk8chosr/image/upload/v1759763607/datem8/kegd8r0qpifrhv8wmvxu.jpg";
+
+        return Scaffold(
+          body: SafeArea(child: _pages[_currentIndex]),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (index) => setState(() => _currentIndex = index),
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: [
+              const BottomNavigationBarItem(
+                  icon: Icon(AppIcons.home), label: ''),
+              const BottomNavigationBarItem(
+                  icon: Icon(AppIcons.chat), label: ''),
+              const BottomNavigationBarItem(
+                  icon: Icon(AppIcons.explore), label: ''),
+              const BottomNavigationBarItem(
+                  icon: Icon(AppIcons.friends), label: ''),
+              BottomNavigationBarItem(
+                icon: CircleAvatar(
+                  radius: 12,
+                  backgroundImage: NetworkImage(imageToShow),
+                  backgroundColor: Colors.grey[300],
+                ),
+                label: '',
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
